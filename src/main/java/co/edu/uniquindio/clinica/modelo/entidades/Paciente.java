af8cc8b84a8c6c157594e6ce29bd5aae0dc4fba1
JDCC1004 <juliand.cruzc@uqvirtual.edu.co>
2023-09-01 00:34:26 -0500

    ClinicaApplication

--- src/main/java/co/edu/uniquindio/clinica/modelo/entidades/Paciente.java
+++ src/main/java/co/edu/uniquindio/clinica/modelo/entidades/Paciente.java
@@ -1,35 +1,28 @@
 package co.edu.uniquindio.clinica.modelo.entidades;
 
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Getter;
+import lombok.*;
 
 import java.io.Serializable;
+import java.util.List;
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Paciente implements Serializable {
-    @Getter
+
     @Id
     private String cedula;
+
     private String nombre;
     private String email;
-    private String telefono;
 
-    public Paciente() {
-        super();
-    }
+    @ElementCollection
+    private List<String> telefono;
 
-    public void setCedula(String cedula) {
-        this.cedula = cedula;
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Paciente paciente = (Paciente) o;
-        return cedula.equals(paciente.cedula);
-    }
-    @Override
-    public int hashCode() {
-        return cedula.hashCode();
-    }
-    }
+}
