1c12cb8c239e152869110739edf606372345a9eb
JDCC1004 <juliand.cruzc@uqvirtual.edu.co>
2023-09-18 10:47:05 -0500

    ClinicaApplication

--- src/main/java/co/edu/uniquindio/clinica/modelo/enums/Especialidad.java
+++ src/main/java/co/edu/uniquindio/clinica/modelo/enums/Especialidad.java
@@ -1,28 +1,12 @@
 package co.edu.uniquindio.clinica.modelo.enums;
 
-import co.edu.uniquindio.clinica.modelo.entidades.Medico;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.io.Serializable;
-import java.util.List;
-
 public enum Especialidad{
 
-    @Id
     OTORRINOLARINGOLOGO,
     CARDIOLOGO,
     GINECOLOGO,
     NEUROLOGO,
     NUTRICIONISTA,
     PEDIATRA;
-
-    @OneToMany(mappedBy = "especialidad")
-    private List<Medico> medico;
-
 
 }
