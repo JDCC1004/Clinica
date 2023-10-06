3474724874b28ce3209c4afcf4c1246295635aac
Julian <juliand.cruzc@uqvirtual.edu.co>
2023-10-06 14:49:55 -0500

    Actualizacion 06/10

--- src/main/java/co/edu/uniquindio/projectClinica/dto/admin/DetalleMedicoDTO.java
+++ src/main/java/co/edu/uniquindio/projectClinica/dto/admin/DetalleMedicoDTO.java
@@ -3,22 +3,38 @@ package co.edu.uniquindio.projectClinica.dto.admin;
 import co.edu.uniquindio.projectClinica.modelo.entidades.Enum.Ciudad;
 import co.edu.uniquindio.projectClinica.modelo.entidades.Enum.Especialidad;
 import co.edu.uniquindio.projectClinica.modelo.entidades.Enum.EstadoUsuario;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 import java.sql.Time;
 import java.time.LocalTime;
 
 public record DetalleMedicoDTO(
 
+        @NotNull @Length(max = 10)
         int codigo,
+        @NotNull @Length(max = 10)
         String cedula,
+        @NotNull @Length(max = 200)
         String nombre,
+        @NotNull @Length(max = 10)
         String telefono,
+        @NotNull @Length(max = 200) @Email
         String correo,
+        @NotNull
         String urlFoto,
+        @NotNull @Min(0) @Max(2)
         Ciudad ciudad,
+        @NotNull @Min(0) @Max(5)
         Especialidad especialidad,
+        @NotNull
         EstadoUsuario estadoUsuario,
+        @NotNull
         LocalTime horaInicio,
+        @NotNull
         LocalTime horaFin
 ) {
 }
--- src/test/java/co/edu/uniquindio/projectClinica/AdministradorTest.java
+++ src/test/java/co/edu/uniquindio/projectClinica/AdministradorTest.java
@@ -1,13 +1,16 @@
 package co.edu.uniquindio.projectClinica;
 
+import co.edu.uniquindio.projectClinica.dto.admin.DetalleMedicoDTO;
 import co.edu.uniquindio.projectClinica.dto.admin.RegistroMedicoDTO;
 import co.edu.uniquindio.projectClinica.modelo.entidades.Enum.Ciudad;
 import co.edu.uniquindio.projectClinica.modelo.entidades.Enum.Especialidad;
+import co.edu.uniquindio.projectClinica.modelo.entidades.Enum.EstadoUsuario;
 import co.edu.uniquindio.projectClinica.servicios.interfaces.AdministradorServicio;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalTime;
 
@@ -18,11 +21,14 @@ public class AdministradorTest {
     @Autowired
     private AdministradorServicio administradorServicio;
     @Test
+    @Sql("classpath:dataset.sql")
     public void crearMedicoTest(){
 
+        DetalleMedicoDTO medi = administradorServicio.obtenerMedico(1);
+
         RegistroMedicoDTO medicoDTO = new RegistroMedicoDTO(
-                "Pepito",
-                "423942",
+                medi.nombre(),
+                medi.cedula(),
                 "238222",
                 Ciudad.ARMENIA,
                 Especialidad.CARDIOLOGO,
@@ -39,4 +45,50 @@ public class AdministradorTest {
             throw new RuntimeException(e);
         }
     }
+
+    @Test
+    public void actualizarMedicoTest(){
+        DetalleMedicoDTO medicoDTO = new DetalleMedicoDTO(
+                10324,
+                "",
+                "238222",
+                "3012980413",
+                "jdcc1004@gmail.com",
+                "url_Foto",
+                Ciudad.ARMENIA,
+                Especialidad.CARDIOLOGO,
+                EstadoUsuario.ACTIVO,
+                LocalTime.of(8, 0),
+                LocalTime.of(12, 0)
+        );
+
+        try {
+            administradorServicio.actualizarMedico(medicoDTO);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @Sql("classpath:dataset.sql")
+    public void eliminarMedico(){
+        administradorServicio.eliminarMedico(2);
+
+        DetalleMedicoDTO medi = administradorServicio.obtenerMedico(2);
+
+
+    }
+
+    /**@Test
+    public void obtenerMedico(){
+        DetalleMedicoDTO Medico = new DetalleMedicoDTO(
+                10324
+        );
+
+        try{
+            administradorServicio.obtenerMedico(Medico);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }*/
 }
