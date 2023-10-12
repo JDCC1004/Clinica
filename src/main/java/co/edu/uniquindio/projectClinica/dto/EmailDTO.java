fdd2ce3ffcb1e404fb21f8b28b0d2348a5359fda
Julian <juliand.cruzc@uqvirtual.edu.co>
2023-10-12 00:37:26 -0500

    Implementación Servicios Externos

--- src/main/java/co/edu/uniquindio/projectClinica/dto/EmailDTO.java
+++ src/main/java/co/edu/uniquindio/projectClinica/dto/EmailDTO.java
@@ -1,4 +1,13 @@
 package co.edu.uniquindio.projectClinica.dto;
 
-public record EmailDTO() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+public record EmailDTO(
+        @Length(max = 80, message = "El asunto no puede tener mas de 80 caracteres")
+        String asunto,
+        String cuerpo,
+        String destinatario
+) {
 }
